b43e9c0662747d678002fcb6e987d53321f2efa6
Rupert Rebentisch <rupert.rebentisch@googlemail.com>
2013-03-08 21:18:29 +0100

    Finished walkthrough. Calculator working.

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -8,11 +8,36 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    BOOL userIsInTheMiddleOfTypingANumber;
+}
 
 @end
 
 @implementation ViewController
+
+-(CalculatorBrain *) brain{
+    if(!brain) brain = [[CalculatorBrain alloc] init];
+    return brain;
+}
+-(IBAction)digitPressed:(UIButton *)sender{
+    NSString * digit = [[sender titleLabel]text];
+    if (userIsInTheMiddleOfTypingANumber){
+        [display setText:[[display text]stringByAppendingString:digit]];
+    }else{
+        [display setText:digit];
+        userIsInTheMiddleOfTypingANumber = YES;
+    }
+}
+-(IBAction)operationPressed:(UIButton *)sender{
+    if(userIsInTheMiddleOfTypingANumber){
+        [[self brain] setOperand:[[display text]doubleValue]];
+        userIsInTheMiddleOfTypingANumber = NO;
+    }
+    NSString * operation = [[sender titleLabel] text];
+    double result = [[self brain] performOperation:operation];
+    [display setText:[NSString stringWithFormat:@"%g",result]];
+}
 
 - (void)viewDidLoad
 {
