2f29786cdace80460214dce2f529edaaf38a561b
rreben <rupert.rebentisch@googlemail.com>
2013-07-06 12:44:18 +0200

    initialized coordinate system with views bounds.

--- Calculator/GraphView.m
+++ Calculator/GraphView.m
@@ -29,8 +29,8 @@
     UILabel *label = [[UILabel alloc] initWithFrame:labelRect];
     [label setText:@"hello"];
     [self addSubview:label];
-    CGRect graphRect = CGRectMake(5, 5, 200, 100);
-    CGPoint origin = CGPointMake(100, 50);
+    CGRect graphRect = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.bounds.size.width, self.bounds.size.height);
+    CGPoint origin = CGPointMake(self.bounds.origin.x + self.bounds.size.width / 2, self.bounds.origin.y + self.bounds.size.height / 2);
     [AxesDrawer drawAxesInRect:graphRect originAtPoint:origin scale:1.0];
 }
 
